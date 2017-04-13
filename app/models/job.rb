a0ebcfe634cf1467824ac4877881cd0c763fec69
王天禹 <wandouba467@gmail.com>
2017-04-13 10:57:03 +0800

    step-6 order by wage

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,9 @@
 class Job < ApplicationRecord
+  scope :published, -> { where(is_hidden: falsse) }
+end
+
+  scope :recent, -> { order('created_at DESC') }
+end
   validates :title, presence: true
   validates :wage_upper_bound, presence: true
   validates :wage_lower_bound, presence: true
